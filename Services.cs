1423f2a3a4d129407533d09f217be2ecbe26d262
SanjeewaDeAlwis <sanj.alwis@gmail.com>
2016-05-06 16:03:48 +0930

    Added files via upload

---
+++ Services.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CANDF.RATES.WCF.SERVICE.LIBRARY;
+
+namespace CANDF.RATES.WEB.SERVICES
+{
+    public class Services
+    {
+        /// <summary>
+        /// Get distance
+        /// </summary>
+        public static double GetDistance(RateRequest request)
+        {
+            DistanceService.ServiceClient service = new DistanceService.ServiceClient();
+            try
+            {
+                double distanceValue = 0;
+                if (request.SenderCountry.ID == 9 && request.ReceiverCountry.ID == 9)
+                {
+                    string distance = service.getAustraliaDistanceByPostCodeandLocation(request.SenderPostCode, request.SenderSuburb.Name, request.ReceiverPostCode, request.ReceiverSuburb.Name).Distance;
+                    double.TryParse(distance, out distanceValue);
+                }
+                return distanceValue;
+            }
+            catch //(Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                service.Close();
+            }
+        }
+    }
+}
